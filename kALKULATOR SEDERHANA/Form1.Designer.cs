e042f957d29f59d7e0a19cb295aae6952d0a53b4
TUF <TUF@LAPTOP-NPVD2F5T>
2024-05-15 14:49:49 +0700

    Add project files.

---
+++ kALKULATOR SEDERHANA/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Kalkulator_Sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn7 = new Button();
+            txtDisplay1 = new TextBox();
+            txtDisplay2 = new TextBox();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn0 = new Button();
+            btnKoma = new Button();
+            btnBagi = new Button();
+            btnKali = new Button();
+            btnKurang = new Button();
+            btnTambah = new Button();
+            btnHasil = new Button();
+            btnClear = new Button();
+            btn6 = new Button();
+            SuspendLayout();
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(12, 69);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(41, 31);
+            btn7.TabIndex = 0;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // txtDisplay1
+            // 
+            txtDisplay1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            txtDisplay1.Location = new Point(12, 12);
+            txtDisplay1.Multiline = true;
+            txtDisplay1.Name = "txtDisplay1";
+            txtDisplay1.Size = new Size(229, 51);
+            txtDisplay1.TabIndex = 1;
+            txtDisplay1.Text = "0";
+            txtDisplay1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // txtDisplay2
+            // 
+            txtDisplay2.BorderStyle = BorderStyle.None;
+            txtDisplay2.Location = new Point(12, 12);
+            txtDisplay2.Name = "txtDisplay2";
+            txtDisplay2.Size = new Size(30, 20);
+            txtDisplay2.TabIndex = 2;
+            txtDisplay2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(59, 69);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(41, 31);
+            btn8.TabIndex = 3;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(106, 69);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(41, 31);
+            btn9.TabIndex = 4;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(12, 106);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(41, 31);
+            btn4.TabIndex = 5;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(59, 106);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(41, 31);
+            btn5.TabIndex = 6;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(12, 143);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(41, 31);
+            btn1.TabIndex = 8;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(59, 143);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(41, 31);
+            btn2.TabIndex = 9;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(106, 143);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(41, 31);
+            btn3.TabIndex = 10;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(12, 180);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(88, 29);
+            btn0.TabIndex = 11;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnKoma
+            // 
+            btnKoma.Location = new Point(106, 180);
+            btnKoma.Name = "btnKoma";
+            btnKoma.Size = new Size(41, 29);
+            btnKoma.TabIndex = 12;
+            btnKoma.Text = ",";
+            btnKoma.UseVisualStyleBackColor = true;
+            // 
+            // btnBagi
+            // 
+            btnBagi.Location = new Point(153, 69);
+            btnBagi.Name = "btnBagi";
+            btnBagi.Size = new Size(41, 31);
+            btnBagi.TabIndex = 13;
+            btnBagi.Text = "/";
+            btnBagi.UseVisualStyleBackColor = true;
+            btnBagi.Click += btnBagi_Click;
+            // 
+            // btnKali
+            // 
+            btnKali.Location = new Point(153, 106);
+            btnKali.Name = "btnKali";
+            btnKali.Size = new Size(41, 31);
+            btnKali.TabIndex = 14;
+            btnKali.Text = "*";
+            btnKali.UseVisualStyleBackColor = true;
+            btnKali.Click += btnKali_Click;
+            // 
+            // btnKurang
+            // 
+            btnKurang.Location = new Point(153, 143);
+            btnKurang.Name = "btnKurang";
+            btnKurang.Size = new Size(41, 31);
+            btnKurang.TabIndex = 15;
+            btnKurang.Text = "-";
+            btnKurang.UseVisualStyleBackColor = true;
+            btnKurang.Click += btnKurang_Click;
+            // 
+            // btnTambah
+            // 
+            btnTambah.Location = new Point(153, 178);
+            btnTambah.Name = "btnTambah";
+            btnTambah.Size = new Size(41, 31);
+            btnTambah.TabIndex = 16;
+            btnTambah.Text = "+";
+            btnTambah.UseVisualStyleBackColor = true;
+            btnTambah.Click += btnTambah_Click;
+            // 
+            // btnHasil
+            // 
+            btnHasil.Location = new Point(200, 141);
+            btnHasil.Name = "btnHasil";
+            btnHasil.Size = new Size(41, 68);
+            btnHasil.TabIndex = 17;
+            btnHasil.Text = "=";
+            btnHasil.UseVisualStyleBackColor = true;
+            btnHasil.Click += btnHasil_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnClear.Location = new Point(200, 69);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(41, 68);
+            btnClear.TabIndex = 18;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(105, 108);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(41, 31);
+            btn6.TabIndex = 19;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(251, 222);
+            Controls.Add(btn6);
+            Controls.Add(btnClear);
+            Controls.Add(btnHasil);
+            Controls.Add(btnTambah);
+            Controls.Add(btnKurang);
+            Controls.Add(btnKali);
+            Controls.Add(btnBagi);
+            Controls.Add(btnKoma);
+            Controls.Add(btn0);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(txtDisplay2);
+            Controls.Add(txtDisplay1);
+            Controls.Add(btn7);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn7;
+        private TextBox txtDisplay1;
+        private TextBox txtDisplay2;
+        private Button btn8;
+        private Button btn9;
+        private Button btn4;
+        private Button btn5;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn0;
+        private Button btnKoma;
+        private Button btnBagi;
+        private Button btnKali;
+        private Button btnKurang;
+        private Button btnTambah;
+        private Button btnHasil;
+        private Button btnClear;
+        private Button btn6;
+    }
+}
